650a3ac3200de0e29615bcb7c969672c85c86f00
jkim574 <30674448+jkim574@users.noreply.github.com>
2020-09-20 18:00:25 -0500

    finished manhattan

--- funny_puzzle.py
+++ funny_puzzle.py
@@ -13,6 +13,18 @@ succ_lookup = {
 # size means n in n x n board
 size = 3
 
+goal_coordinates = {
+	1: (0, 0),
+	2: (0, 1),
+	3: (0, 2),
+	4: (1, 0),
+	5: (1, 1),
+ 	6: (1, 2),
+ 	7: (2, 0),
+	8: (2, 1),
+	0: (2, 2)
+}
+
 
 def get_r_c_tuple(index):
 	"""Given a list index, return a tuple of (row, column)
@@ -38,18 +50,48 @@ def swap(state, src, dst):
 
 	#print(f'After swap: {state}')
 
+
+
+def manhattan(curr_coordinate, goal_coordinate):
+	"""Calculate the manhattan distance from current coordinate (r,c) to goal coordinate (r,c)."""
+	r1 = curr_coordinate[0] 
+	c1 = curr_coordinate[1]
+
+	r2 = goal_coordinate[0]
+	c2 = goal_coordinate[1]
+
+	distance = abs(r2 - r1) + abs(c2 - c1)
+	return distance 
+
+
+def get_manhattan_distance(state):
+	"""Given a state (1D Python List), calculate its manhattan distance value by summing up the manhattan distance of each tile
+	"""
+	total_sum = 0
+	#print(f'State: {state}')
+	for idx, tile in enumerate(state):
+		if tile == 0:
+			continue
+		curr_coordinate = get_r_c_tuple(idx)
+		goal_coordinate = goal_coordinates[tile]
+		h = manhattan(curr_coordinate, goal_coordinate)	
+		#print(f'Tile {tile}: {curr_coordinate} -> {goal_coordinate} h = {h}')
+		total_sum += h
+	return total_sum
+
+
 def print_succ(state):
 	# Find the index of 0 from state (Python list)
 	idx = state.index(0)
-	print(f'index: {idx}')
+#	print(f'index: {idx}')
 
 	# Convert the index to (r, c) tuple
 	index_tuple = get_r_c_tuple(idx)
-	print(f'index tuple: {index_tuple}')
+#	print(f'index tuple: {index_tuple}')
 
 	# Look up successor indices from succ_lookup dictionary by using the (r, c) tuple
 	succ_indices = succ_lookup[index_tuple]
-	print(f'sucessor index tuples: {succ_indices}')	
+#	print(f'sucessor index tuples: {succ_indices}')	
 	
 	succ_list = []
 	# For each successor index, create a copy of state and swap the 0
@@ -58,10 +100,15 @@ def print_succ(state):
 		swap(state_cp, index_tuple, swap_idx)	
 		succ_list.append(state_cp)
 	
-	sort_succ_list = sorted(succ_list)
+	sorted_succ_list = sorted(succ_list)
 
-	for item in sort_succ_list:
-		print(item)
+	#for item in sorted_succ_list:
+	#	print(item)
+
+	for succ in sorted_succ_list:
+		h = get_manhattan_distance(succ)
+		print(f'{succ} h={h}')
+
 
 
 def solver(state):
@@ -77,7 +124,7 @@ def main():
 		rc = get_r_c_tuple(i)
 		print(rc)
 	'''
-	swap(state, (2,1), (1,1))
+
 
 if __name__ == '__main__':
 	main()
